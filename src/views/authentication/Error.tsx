d4f61ccd07adbe36e35398280782892fbca7396c
mehrannj <mehran.najafi1988@gmail.com>
2025-07-23 13:27:15 +0330

    درست کردن 404
    درست کردن مسیر 404 هنگام بررسی توکن

--- src/views/authentication/Error.tsx
+++ src/views/authentication/Error.tsx
@@ -1,34 +1,50 @@
+import React, { useEffect } from 'react';
 import { Box, Container, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import ErrorImg from 'src/assets/images/backgrounds/errorimg.svg';
 
-const Error = () => (
-  <Box
-    display="flex"
-    flexDirection="column"
-    height="100vh"
-    textAlign="center"
-    justifyContent="center"
-  >
-    <Container maxWidth="md">
-      <img src={ErrorImg} alt="404" />
-      <Typography align="center" variant="h1" mb={4}>
-        Opps!!!
-      </Typography>
-      <Typography align="center" variant="h4" mb={4}>
-        This page you are looking for could not be found.
-      </Typography>
-      <Button
-        color="primary"
-        variant="contained"
-        component={Link}
-        to="/dashboards/modern"
-        disableElevation
-      >
-        Go Back to Home
-      </Button>
-    </Container>
-  </Box>
-);
+const Error = () => {
+  const navigate = useNavigate(); // Get the navigate function
+
+  const handleGoBack = () => {
+    const authToken = localStorage.getItem('authToken'); // Check for the token
+
+    if (authToken) {
+      // If token exists, navigate to the dashboard
+      navigate('/dashboards/dashboard');
+    } else {
+      // If token does not exist, navigate to the login page
+      navigate('/auth/login');
+    }
+  };
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      height="100vh"
+      textAlign="center"
+      justifyContent="center"
+    >
+      <Container maxWidth="md">
+        <img src={ErrorImg} alt="404" />
+        <Typography align="center" variant="h1" mb={4}>
+          Eyvah!
+        </Typography>
+        <Typography align="center" variant="h4" mb={4}>
+          Aradığınız sayfa bulunamadı
+        </Typography>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={handleGoBack} // Use onClick to trigger our conditional logic
+          disableElevation
+        >
+          Ana Sayfaya Geri Dön
+        </Button>
+      </Container>
+    </Box>
+  );
+};
 
 export default Error;
